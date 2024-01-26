8496efce6e6c5804590871fea9511225b66288fb
gp3174787533 <56432602+gp3174787533@users.noreply.github.com>
2024-01-26 16:45:14 +0800

    init

---
+++ dialogue/src/main/java/com/dialogue/gpt/DialogueApplication.java
@@ -0,0 +1,12 @@
+package com.dialogue.gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialogueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DialogueApplication.class, args);
+    }
+
+}
---
+++ dialogue/src/test/java/com/dialogue/gpt/DialogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dialogue.gpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
